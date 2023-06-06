34767ef462ea6af38b4b0eee90fdde0a4ae61bc3
Yeeun <kye3314@gmail.com>
2023-06-06 15:57:01 +0900

    예약페이지 쿼리수정

--- page_reservation_delete.php
+++ page_reservation_delete.php
@@ -7,10 +7,5 @@ $sql = "delete from reservation where licenseplateno=:license";
 $sql = $conn->prepare($sql);
 $sql->bindParam(':license', $license);
 $sql->execute();
-$sql = "delete from previousrental where licenseplateno=:license";
-$sql = $conn->prepare($sql);
-$sql->bindParam(':license', $license);
-$sql->execute();
-echo ("<SCRIPT>location.href='page_reservation.php?';</SCRIPT>");
 exit;
 ?>
